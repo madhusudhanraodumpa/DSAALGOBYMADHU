988af6baba9d8f48d8fb17623844899c5ab8b420
madhusudhanraodumpa <madhu.sudhan40@gmail.com>
2019-10-31 01:20:39 +0530

    smallest subarray of size

---
+++ src/com/org/learnby/array/MinmumSubsetOfArray.java
@@ -0,0 +1,42 @@
+package com.org.learnby.array;
+
+public class MinmumSubsetOfArray {
+
+    public static void main(String args[]) {
+        int[] a =   {1, 4, 45, 6, 0, 19,52};
+
+        int f = 0, r = 0;
+        int sum = 51;
+        int currentsum = 0;
+        int minSubset = Integer.MAX_VALUE;
+
+        for (int i = 0; i < a.length; i++) {
+            currentsum = a[i] + currentsum;
+            r++;
+            if (currentsum < sum) {
+                if(currentsum<=0 && sum>0){
+                    f=r;
+                    currentsum=0;
+                }
+                continue;
+            } else {
+                if (minSubset > (r - f)) {
+                    minSubset = r - f;
+                }
+                while (currentsum - a[f] > sum) {
+                    currentsum = currentsum - a[f++];
+                    if (minSubset > (r - f)) {
+                        minSubset = r - f;
+                    }
+                }
+
+            }
+
+
+        }
+        if(minSubset>a.length){
+            System.out.println("Not possible");
+        }
+        System.out.println(minSubset);
+    }
+}
